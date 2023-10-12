075ad22c4ed013c1affca60137adaefded2f440f
ahmedqadar <abdulnet029@gmail.com>
2023-10-12 20:40:26 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputField = document.querySelector('#new-task-description');
+    uploadToDo(inputField.value);
+    inputField.value = '';
+  })
 });
+
+function uploadToDo(to_do){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', () => {
+    p.remove();
+  });
+  btn.textContent = 'x';
+  p.textContent = `${to_do} `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
